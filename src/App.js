40aa33bc8099481c9fb08e32d2e1ca3d8891c423
Rajatbisht12 <500095841@stu.upes.ac.in>
2024-05-04 20:34:00 +0530

    restart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import CourseList from './components/CourseList';
-import CourseDetails from './components/CourseDetails';
-import StudentDashboard from './components/StudentDashboard';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<CourseList />} />
-            <Route path="/course/:id" element={<CourseDetails />} />
-            <Route path="/student-dashboard" element={<StudentDashboard />} />
-          </Routes>
-        </div>
-      </Router>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-
 
 export default App;
